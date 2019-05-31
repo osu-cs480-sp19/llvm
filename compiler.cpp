6fd3a5867689c4a2bcee6a2d19e371973b60f722
Rob Hess <robwhess@gmail.com>
2019-05-31 11:45:52 -0700

    Add code from 5/31 lecture.

--- compiler.cpp
+++ compiler.cpp
@@ -1,5 +1,40 @@
 #include <iostream>
 
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Verifier.h"
+
+// using namespace llvm;
+
+static llvm::LLVMContext TheContext;
+static llvm::IRBuilder<> TheBuilder(TheContext);
+static llvm::Module* TheModule;
+
 int main(int argc, char const *argv[]) {
+  TheModule = new llvm::Module("LLVM_Demo", TheContext);
+
+  llvm::FunctionType* fooFnType = llvm::FunctionType::get(
+    llvm::Type::getVoidTy(TheContext), false
+  );
+
+  llvm::Function* fooFn = llvm::Function::Create(
+    fooFnType,
+    llvm::GlobalValue::ExternalLinkage,
+    "foo",
+    TheModule
+  );
+
+  llvm::BasicBlock* entryBlock =
+    llvm::BasicBlock::Create(TheContext, "entry", fooFn);
+  TheBuilder.SetInsertPoint(entryBlock);
+
+  TheBuilder.CreateRetVoid();
+
+
+  llvm::verifyFunction(*fooFn);
+  TheModule->print(llvm::outs(), NULL);
+
+  delete TheModule;
   return 0;
 }
